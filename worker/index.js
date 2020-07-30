677155436fbd4440f141323be3981d5a2c670dbd
Marhatta <vishal.marhatta@gmail.com>
2020-07-30 17:50:50 +0530

    :art: Added comments

--- worker/index.js
+++ worker/index.js
@@ -1,39 +1,38 @@
 "use strict";
 
-import { openDB, deleteDB, wrap, unwrap } from "idb";
+import { openDB } from "idb";
 
-
-const CACHE_VERSION = 66;
+const CACHE_VERSION = 1;
 const CACHE_STATIC_NAME = `simple-cache-v${CACHE_VERSION}`;
 const CACHE_DYNAMIC_NAME = `dynamic-cache-v${CACHE_VERSION}`;
-const CACHE_APISTORE_NAME = `dynamic-cache-api-v${CACHE_VERSION}`;
 
-const urlsToCache = ["/", "/offline", "/index", "/newPage"];
+/** URLS to be cached
+ * (Static Caching)
+ * Caches assets when PWA is installed
+ */
+const urlsToCache = ["/", "/offline", "/index"];
 
-// indexdb caching...
+/** Create IndexDb Store
+ * Open IndexDb to perform operations
+ */
 let db;
 (function dbOperation() {
-  console.log("running url index db ...");
-
   async function createDB() {
     db = await openDB("ApiDB", 1, {
       upgrade(db) {
-        // Create a store of objects
         const store = db.createObjectStore("apistore", {
-          // The 'id' property of the object will be the key.
           keyPath: "url",
-          // If it isn't explicitly set, create a value by auto incrementing.
-          // autoIncrement: true,
         });
-        // Create an index on the 'date' property of the objects.
-        // store.createIndex('date', 'date');
       },
     });
   }
   createDB();
-  console.log("store ok ");
 })();
 
+/**
+ * Install service worker
+ * #urlsToCache are cached here
+ */
 self.addEventListener("install", (event) => {
   console.log("Service worker registered");
   const preLoaded = caches
@@ -42,10 +41,13 @@ self.addEventListener("install", (event) => {
   event.waitUntil(preLoaded);
 });
 
+/**
+ * Activating service worker
+ * Remember to update the CACHE_VERSION while making any changes in the service worker
+ */
 self.addEventListener("activate", function (event) {
   console.log("[Service Worker] Activating Service Worker ....", event);
-
-  // add something to index db
+  /**Clears the older cache i.e already existing cache */
   event.waitUntil(
     caches.keys().then(function (keyList) {
       return Promise.all(
@@ -61,17 +63,26 @@ self.addEventListener("activate", function (event) {
   return self.clients.claim();
 });
 
-// handle network request
+/**Listens for any incoming request
+ * Makes the actual request
+ * Gets the response back
+ * Response can be modified here
+ * And then passed back to client app
+ */
+
+/**
+ * NETWORK FIRST CACHE STRATEGY
+ * Giving preference to network
+ */
 self.addEventListener("fetch", (event) => {
   let reqUrl = event.request.url;
-
-  console.log("REQ FROM SW :->URL :", reqUrl);
-  const apiUrlPtr = 'jsonplaceholder'
-  if (isreqUrl.includes(apiUrlPtr)) {
+  const apiUrlPtr = "jsonplaceholder";
+  /** Store in index db for particular URLs */
+  if (reqUrl.includes(apiUrlPtr)) {
     event.respondWith(
       fetch(event.request)
         .then(function (res) {
-          // store  the request res to indexdb
+          /**Store the api response in index db */
           if (res.status == 200) {
             res
               .clone()
@@ -83,43 +94,37 @@ self.addEventListener("fetch", (event) => {
                 });
               });
           }
-
           return res;
         })
         .catch(async function (err) {
-          console.log("return from offline indexdb api===> ");
-          console.log(" NETWORK FAIL API -> OFFLINE CACHE ");
-
-          // return from indexdb store
+          /**
+           * If offline, sends back the data from indexdb
+           */
           return db.get("apistore", event.request.url).then((res) => {
-            console.log("================", res);
             return new Response(JSON.stringify(res.body));
-
-            // return res.body;
           });
         })
     );
   } else {
-    // NETWORK FIRST CACHE STRATERGY
+    /**fetch the request and store in cache */
     event.respondWith(
       fetch(event.request)
         .then(function (res) {
-          console.log("CACHING ...TO LOCAL FROM NETWORK", event.request.url);
           return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+            /**Remember to clone the response
+             * As you can use "res" object only once
+             */
             cache.put(event.request.url, res.clone());
-            // update the cache and return the network res;
             return res;
           });
         })
         .catch(function (err) {
-          console.log("NETWORK FAIL GENERAL -> OFFLINE CACHE");
-
+          /**Serve response from cache */
           return caches.match(event.request.url).then((res) => {
-            console.log("CHECKING FOR OFFLINE CACHE RESPONSE : ", res);
             if (res) {
               return res;
             } else {
-              console.log("No offline chache Res!");
+              /** If nothing found in cache as well, render a 404 page */
               return caches.match("/offline").then((res) => {
                 return res;
               });
